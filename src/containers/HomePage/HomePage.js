3e973dc8277cba06cf355ab96b69ff371ecc70c7
Tinh0378277554 <thuantinh879@gmail.com>
2024-06-28 23:11:53 +0700

    44 25

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class HomePage extends Component {
+    render() {
+
+        return (
+            <div> Hello HomePage </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
