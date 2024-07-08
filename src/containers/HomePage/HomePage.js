efb9fa1781c0c3f52be58355a1e55e1529683aa2
Tinh0378277554 <thuantinh879@gmail.com>
2024-07-08 13:57:29 +0700

    48 tích hợp thư viện

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import HomeHeader from './HomeHeader';
+import Specialty from "./Section/Specialty";
+import { head } from "lodash";
 
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <HomeHeader />
+                <Specialty />
+                <div style={{ height: '300px' }}></div>
             </div>
         );
     }
