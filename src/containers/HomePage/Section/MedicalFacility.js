0d7cfc28434e381da52c1599e9b87ee6a04d42e4
Tinh0378277554 <thuantinh879@gmail.com>
2024-07-13 22:57:47 +0700

    50 comple interface clone BookingCare

--- src/containers/HomePage/Section/MedicalFacility.js
+++ src/containers/HomePage/Section/MedicalFacility.js
@@ -1,46 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import './Specialty.scss';
+import "./MedicalFacility.scss"
 import { FormattedMessage } from 'react-intl';
 import Slider from "react-slick";
 
-
-class Specialty extends Component {
-
+class MedicalFacility extends Component {
     render() {
-
         return (
-            <div className="section-share section-specialty">
+            <div className="section-share section-medical-facility">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
+                        <span className="title-section">Cơ sở y tế nổi bậc</span>
                         <button className="btn-section">Xem thêm</button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty" />
-                                <div>Cơ xương khớp 1</div>
+                                <div className="bg-image section-medical-facility" />
+                                <div>Hệ thống y tế thu cúc 1</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty" />
-                                <div>Cơ xương khớp 2</div>
+                                <div className="bg-image section-medical-facility" />
+                                <div>Hệ thống y tế thu cúc 2</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty" />
-                                <div>Cơ xương khớp 3</div>
+                                <div className="bg-image section-medical-facility" />
+                                <div>Hệ thống y tế thu cúc 3</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty" />
-                                <div>Cơ xương khớp 4</div>
+                                <div className="bg-image section-medical-facility" />
+                                <div>Hệ thống y tế thu cúc 4</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty" />
-                                <div>Cơ xương khớp 5</div>
+                                <div className="bg-image section-medical-facility" />
+                                <div>Hệ thống y tế thu cúc 5</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty" />
-                                <div>Cơ xương khớp 6</div>
+                                <div className="bg-image section-medical-facility" />
+                                <div>Hệ thống y tế thu cúc 6</div>
                             </div>
                         </Slider>
                     </div>
@@ -54,13 +51,11 @@ class Specialty extends Component {
 const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-    };
+    return {};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(MedicalFacility);
