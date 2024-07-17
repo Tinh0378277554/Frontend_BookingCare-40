0793170ab7adaaf3fe2c15992fe87fad8dc87c34
Tinh0378277554 <thuantinh879@gmail.com>
2024-07-17 18:05:22 +0700

    54 redux and localstored

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -13,7 +13,7 @@ class About extends Component {
                 </div>
                 <div className="section-about-content">
                     <div className="content-left">
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/OASGscJQXp0" title="BookingCare: Hệ thống đặt khám trực tuyến" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/OASGscJQXp0" title="BookingCare: Hệ thống đặt khám trực tuyến" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                     </div>
                     <div className="content-right">
                         <p>BookingCare là Nền tảng Y tế chăm sóc sức khỏe toàn diện hàng đầu Việt Nam kết nối người dùng với trên 200 bệnh viện - phòng khám uy tín, hơn 1,500 bác sĩ chuyên khoa giỏi và hàng nghìn dịch vụ, sản phẩm y tế chất lượng cao.</p>
